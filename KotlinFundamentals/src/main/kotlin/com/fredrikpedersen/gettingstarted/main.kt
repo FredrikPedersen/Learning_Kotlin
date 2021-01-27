08ee29abdf2586c9282b08d336046f06407792dc
Fredrik Pedersen <fredrikhp@gmail.com>
2021-01-27 10:08:44 +0100

    Completed Part 3: Section 5: Using "if" as an Expression

--- KotlinFundamentals/src/main/kotlin/com/fredrikpedersen/gettingstarted/main.kt
+++ KotlinFundamentals/src/main/kotlin/com/fredrikpedersen/gettingstarted/main.kt
@@ -3,14 +3,14 @@ fun main(args: Array<String>) {
     val question = Question()
     question.answer = "42"
     question.display()
-    println("The answer is ${question.answer}")
 }
 
 class Question {
     var answer: String = ""
+    val correctAnswer = "42"
     val question: String = "What is the answer to life, the universe and everything?"
 
     fun display() {
-        println("Q: $question + \nA: $answer")
+        println("Question: $question\nYou answered: $answer, which is ${if (answer == correctAnswer) "correct" else "wrong"}")
     }
 }
