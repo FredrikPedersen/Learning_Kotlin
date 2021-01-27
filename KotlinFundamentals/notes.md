99814aa1c5c6af7f5496017bdd2ee9541f66685c
Fredrik Pedersen <fredrikhp@gmail.com>
2021-01-27 10:22:10 +0100

    Completed Part 3: Section 6: How Kotlin Improves the Handling of Null Values

--- KotlinFundamentals/notes.md
+++ KotlinFundamentals/notes.md
@@ -9,4 +9,16 @@
  
  ### Part 3 - Getting Started With Kotlin
  
+  - ?-operator on a value indicates that it can be null
+  - ?-operator on a method call functions as a null check on the object calling the function, safely performing the operation even if the object is null.
  
+ ```Kotlin
+ 
+	//text initialized to null, call to .length outputs null.
+	val text: String? = null
+	print(text?.length)
+	
+	//text initialized to String-value, call to .length outputs 11
+	val text: String? = "Hello There"
+	print(text?.length)
+ ````
--- KotlinFundamentals/src/main/kotlin/com/fredrikpedersen/gettingstarted/main.kt
+++ KotlinFundamentals/src/main/kotlin/com/fredrikpedersen/gettingstarted/main.kt
@@ -1,16 +1,24 @@
 fun main(args: Array<String>) {
 
-    val question = Question()
+    val question: Question = Question()
     question.answer = "42"
-    question.display()
+    question.solution()
 }
 
 class Question {
-    var answer: String = ""
+    var answer: String? = null
     val correctAnswer = "42"
     val question: String = "What is the answer to life, the universe and everything?"
 
-    fun display() {
-        println("Question: $question\nYou answered: $answer, which is ${if (answer == correctAnswer) "correct" else "wrong"}")
+    fun solution() {
+
+        if (answer != null) {
+            val message =
+                "Question: $question\nYou answered: $answer, which is ${if (answer == correctAnswer) "correct" else "wrong"}"
+            println(message)
+        }
+        else {
+            println("You haven't answered yet!")
+        }
     }
 }
