fb002dad77780b75888a246f1a43e2be5421ee7c
Fredrik Pedersen <fredrikhp@gmail.com>
2021-05-24 12:57:50 +0200

    Removed code from Pluralsight course.

--- KotlinFundamentals/src/main/kotlin/com/fredrikpedersen/gettingstarted/Main.kt
+++
@@ -1,9 +0,0 @@
-fun main(args: Array<String>) {
-    val question = Question()
-    question.answer = 42
-    println(question.solution())
-
-    question.answer = 69
-    println(question.solution())
-}
-
