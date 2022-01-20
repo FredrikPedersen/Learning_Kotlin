6a1fdb8e3a622e7011547841503ee409b8b97927
Fredrik Pedersen <fredrikhp@gmail.com>
2022-01-20 12:52:40 +0100

    Cleanup

--- KotlinForJavaDevelopers/src/main/kotlin/com/fredrikpedersen/testing/Main.kt
+++ KotlinForJavaDevelopers/src/main/kotlin/com/fredrikpedersen/testing/Main.kt
@@ -1,10 +1,4 @@
 package com.fredrikpedersen.testing
 
 fun main(args: Array<String>) {
-
-    println("this is all in lowercase".capitalize())
-}
-
-fun String.capitalize(): String {
-    return this.substring(0, 1).uppercase() + this.substring(1)
 }
